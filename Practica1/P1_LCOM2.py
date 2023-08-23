06526d445b08712c6ee35e4d03d9517b4936b099
AbimaelFranco <82774288+AbimaelFranco@users.noreply.github.com>
2023-08-23 12:05:12 -0600

    Practica 1 terminada

--- Practica1/P1_LCOM2.py
+++ Practica1/P1_LCOM2.py
@@ -1,35 +1,46 @@
 import numpy as np
 
+# Función para realizar el cifrado César
 def cifrado_cesar(texto, clave):
-    resultado = []
+    resultado = []  # Lista para almacenar el resultado cifrado
     for letra in texto:
-        if letra.isalpha():
+        if letra.isalpha():  # Verixxfica si la letra es parte del alfabeto
             offset = ord('a') if letra.islower() else ord('A')
+            # Aplica el desplazamiento y módulo 26 para mantenerlo en el rango de letras
             cifrada = chr((ord(letra) - offset + clave) % 26 + offset)
-            resultado.append(cifrada)
+            resultado.append(cifrada)  # Agrega la letra cifrada al resultado
         else:
-            resultado.append(letra)
-    return ''.join(resultado)
+            resultado.append(letra)  # Si no es letra, la agrega sin cambios al resultado
+    return ''.join(resultado)  # Convierte la lista en una cadena y devuelve el resultado
 
+# Función para realizar el descifrado César
 def descifrado_cesar(texto, clave):
-    return cifrado_cesar(texto, -clave)
+    return cifrado_cesar(texto, -clave)  # Llama a la función cifrado_cesar con clave negativa
 
-def cifrado_hill(texto, clave):
-    clave_matrix = np.array(clave)
-    texto_numeros = [ord(letra) - ord('a') for letra in texto.lower() if letra.isalpha()]
-    while len(texto_numeros) % clave_matrix.shape[0] != 0:
-        texto_numeros.append(0)
-    texto_matrix = np.array(texto_numeros).reshape(-1, clave_matrix.shape[0])
-    resultado_matrix = np.dot(texto_matrix, clave_matrix) % 26
-    cifrado = ''.join(chr(num + ord('a')) for fila in resultado_matrix for num in fila)
-    return cifrado
+# Función para convertir una matriz numérica en una cadena de letras
+def matrix_to_letter(n):
+    global encry_string
+    encry_string = ''
+    for i in n:
+        for j in i:
+            encry_string += chr(j + 97)  # Convierte el valor a letra (a=0, b=1, ...) y agrega al resultado
+    return encry_string 
 
-def descifrado_hill(texto, clave):
-    clave_matrix = np.array(clave)
-    clave_inverse = np.linalg.inv(clave_matrix)
-    clave_inverse = np.round(clave_inverse * np.linalg.det(clave_matrix)) % 26
-    return cifrado_hill(texto, clave_inverse)
+# Función para realizar el cifrado Hill
+def cifrado_hill(a, b):
+    encry_l = []  # Lista para almacenar el resultado cifrado
+    for i in range(0, len(b), 2):  # Divide el texto en bloques de dos letras
+        l = []
+        try:
+            l += [ord(b[i]) - 97, ord(b[i + 1]) - 97]  # Convierte las letras a números (a=0, b=1, ...)
+        except:
+            l += [ord(b[i]) - 97, 0]  # Si solo queda una letra en el bloque, rellena con cero
+        c = np.matmul(a, l)  # Realiza multiplicación matricial entre la matriz y el bloque de letras
+        encry_l.append(c % 26)  # Agrega el resultado a la lista (módulo 26 para mantener en el rango)
+    
+    return matrix_to_letter(encry_l)  # Convierte la lista en una cadena de letras y devuelve el resultado
 
+# Función que muestra un menú interactivo al usuario
 def menu():
     while True:
         print("\nSelecciona un tipo de operación:")
@@ -41,8 +52,8 @@ def menu():
         opcion = input("Ingresa el número de la opción: ")
 
         if opcion == '5':
-                print("¡Hasta luego!")
-                break
+            print("¡Hasta luego!")
+            break
 
         if opcion == '1' or opcion == '2':
             texto = input("Ingresa el texto: ")
@@ -51,18 +62,37 @@ def menu():
                 resultado = cifrado_cesar(texto, clave)
             else:
                 resultado = descifrado_cesar(texto, clave)
+
         elif opcion == '3' or opcion == '4':
-            texto = input("Ingresa el texto: ")
-            clave_str = input("Ingresa la clave como una matriz 2x2 separada por comas y espacios (ejemplo: 2, 3, 5, 7): ")
+            msg = input("Ingresa el texto: ")
+            msj = "Ingresa la clave como una matriz 2x2 separada por comas ejemplo (1, 2, 1, 3): "
+            clave_str = input(msj)
             clave = [int(x.strip()) for x in clave_str.split(',')]
-            clave = np.array(clave).reshape(2, 2)
+
+            word_list = msg.split(" ")  # Divide el texto en palabras
+            mensaje_desencriptado = ''
+            mensaje_encriptado = ''
+
             if opcion == '3':
-                resultado = cifrado_hill(texto, clave)
+                for i in word_list:
+                    length = len(i)
+                    if len(i)%2!=0:
+                        i=i+'a'
+                    randmat = np.array(clave).reshape(2, 2)  # Crea la matriz clave
+                    mensaje_encriptado += " " + cifrado_hill(randmat, i)  # Realiza el cifrado Hill
+                resultado = mensaje_encriptado
             else:
-                resultado = descifrado_hill(texto, clave)
+                for i in word_list:
+                    length = len(i)
+                    if len(i)%2!=0:
+                        i=i+'a'
+                    randmat = np.array(clave).reshape(2, 2)  # Crea la matriz clave
+                    inv = np.linalg.inv(randmat)  # Calcula la inversa de la matriz
+                    int_inv = inv.astype(int)  # Convierte los valores de la matriz inversa a enteros
+                    mensaje_desencriptado += " " + cifrado_hill(int_inv, i)[0:length]  # Realiza el descifrado Hill
+                resultado = mensaje_desencriptado
         else:
-            print("Opción inválida.")
-            return
+            resultado = "Opción inválida."
         
         print("Resultado:", resultado)
 
